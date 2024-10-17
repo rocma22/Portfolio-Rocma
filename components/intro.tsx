e90f012e2d754c8b62ac98cfc74410501a898eae
rocma22 <amine99hasnat@gmail.com>
2024-10-17 15:50:18 +0100

    edit the intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -49,7 +49,7 @@ export default function Intro() {
                 <span className="font-bold">full-stack developer</span> with{" "}
                 <span className="font-bold">4 years</span> of experience. I enjoy
                 building <span className="italic">sites & apps</span>. My focus is{" "}
-                <span className="underline">React (Next.js &amp;&amp; vite)</span>.
+                <span className="underline">React (Next.js &amp; vite)</span>.
             </motion.h1>
 
             <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
