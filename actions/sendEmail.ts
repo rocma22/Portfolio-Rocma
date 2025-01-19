4d9b2e03fe1ed73fa3bdf06180f82fb175dfa9b6
rocma22 <amine99hasnat@gmail.com>
2025-01-19 17:24:45 +0100

    edit email

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -27,7 +27,7 @@ export const sendEmail = async (formData: FormData) => {
     try {
         data = await resend.emails.send({
             from: "Contact Form <onboarding@resend.dev>", // Use a verified domain email
-            to: "amine99hasnat@gmail.com", // You can use any email after verifying the domain
+            to: "amine.hassnat@gmail.com", // You can use any email after verifying the domain
             subject: "Message from contact form",
             reply_to: senderEmail as string,
             react: React.createElement(ContactFormEmail, {
--- components/contact.tsx
+++ components/contact.tsx
@@ -45,7 +45,7 @@ export default function Contact() {
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
         <a className="underline" href="mailto:amine99hasnat@gmail.com">
-          amine99hasnat@gmail.com
+          amine.hassnat@gmail.com
         </a>{" "}
         or through this form.
       </p>
